8d83239873bf1edce7ae5d81cea0617332e709e2
Laure-Anne Bluteau <laure-anne.bluteau@etu.univ-grenoble-alpes.fr>
2024-10-01 23:04:08 +0200

    Implémentation des classes QueueBroker et MessageQueue + modification Task

--- Task2/src/Implem/AbstractTask.java
+++ Task2/src/Implem/AbstractTask.java
@@ -3,10 +3,16 @@ package Implem;
 public abstract class AbstractTask extends Thread {
 	Broker broker;
 	Runnable runnable;
+	QueueBroker qb;
 	
 	AbstractTask(Broker b, Runnable r) {
 		this.broker = b;
 		this.runnable = r;
 		
+	}
+
+	public AbstractTask(QueueBroker qb, Runnable r) {
+		this.qb = qb;
+		this.runnable = r;
 	};
 }
--- Task2/src/Implem/Task.java
+++ Task2/src/Implem/Task.java
@@ -5,19 +5,28 @@ public class Task extends AbstractTask {
 	Broker broker;
 	Runnable r;
 
-    Task(Broker b, Runnable r) {
+    public Task(Broker b, Runnable r) {
         super(b, r);
         this.broker = b;
         this.r = r;
+    }
+    
+    public Task(QueueBroker qb, Runnable r) {
+    	super(qb, r);
     }
     
     public void run() {
     	r.run();
     }
     
-    static Broker getBroker() {
+    public static Broker getBroker() {
     	AbstractTask t = (AbstractTask) currentThread();
 		return t.broker;
+    }
+    
+    public static QueueBroker getQueueBroker() {
+    	AbstractTask t = (AbstractTask) currentThread();
+		return (QueueBroker)t.qb;
     }
 
 }
